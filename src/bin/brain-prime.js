56e7fdaaaf3f9c0668beea789bb24c1745edc82a
grom194 <brskoshelev@gmail.com>
2017-05-21 11:08:25 +0300

    task_9

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import newGame from '../games/brain-prime';
+
+newGame();
---
+++ src/games/brain-prime.js
@@ -0,0 +1,29 @@
+import getRandomNum from '../functions';
+import startGame from '..';
+
+const createQuestion = () => {
+  const RandomN = getRandomNum(1, 20);
+
+  const isPrime = (num, index) => {
+    if (index >= num - 1) return 'yes';
+    if (num % index === 0) return 'no';
+    return isPrime(num, index + 1);
+  };
+  const rightAnswer = isPrime(RandomN, 2);
+
+  return {
+    text: RandomN,
+    answer: rightAnswer,
+  };
+};
+
+const getGameRules = () => 'Is it a prime number?';
+
+export default () => {
+  const functions = {
+    GameRules: getGameRules(),
+    createQuestion,
+  };
+  startGame(functions);
+  return true;
+};
