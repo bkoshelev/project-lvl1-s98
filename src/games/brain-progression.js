f773e1f16fcdf90d325e3e92ecab14f891b217d4
grom194 <brskoshelev@gmail.com>
2017-05-21 10:26:10 +0300

    task_8

---
+++ src/games/brain-progression.js
@@ -0,0 +1,34 @@
+import getRandomNum from '../functions';
+import startGame from '..';
+
+const createQuestion = () => {
+  const firstNum = [getRandomNum(1, 20)];
+  const step = getRandomNum(1, 4);
+  const missNum = getRandomNum(0, 9);
+
+  const createStepProgr = (result, index) => {
+    if (index === 10) return result;
+    result.push(result[result.length - 1] + step);
+    return createStepProgr(result, index + 1);
+  };
+  const newProgr = createStepProgr(firstNum, 0);
+
+  const rightAnswer = newProgr[missNum];
+  newProgr[missNum] = '..';
+
+  return {
+    text: newProgr.join(' '),
+    answer: String(rightAnswer),
+  };
+};
+
+const getGameRules = () => 'What number is missing in this progression?';
+
+export default () => {
+  const functions = {
+    GameRules: getGameRules(),
+    createQuestion,
+  };
+  startGame(functions);
+  return true;
+};
